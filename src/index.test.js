e9119ead973419dfad1249ad6bb64bdc1aef26b5
Christopher A. Sesugh <codingsimba@Christophers-MacBook-Pro.local>
2024-07-27 12:08:45 +0100

    add changes

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,20 @@
 import { it, expect, describe } from "vitest";
 describe("Test suite 1 ", () => {
-  it("should work", () => {
-    expect(1).toBe(1);
+  it("should work 1", () => {
+    expect(1).toEqual(1);
   });
 
-  it("should work", () => {
-    expect(1).toBe(2);
+  it("should work 2", () => {
+    expect(1).toEqual(2);
   });
 });
 
 describe("Test suite 2 ", () => {
-  it("should work", () => {
-    expect(1).toBe(1);
+  it("should work 3", () => {
+    expect(1).toEqual(1);
   });
 
-  it("should work", () => {
-    expect(1).toBe(4);
+  it("should work 4", () => {
+    expect(1).toEqual(4);
   });
 });
