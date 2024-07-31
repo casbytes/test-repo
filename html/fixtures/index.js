a1c650008a7510116f250bc92e7be4039c55b473
christophersesugh <christohybrid185@gmail.com>
2024-07-31 12:49:09 +0100

    restructure

---
+++ html/fixtures/index.js
@@ -0,0 +1,2 @@
+import { add } from "../src/index.js";
+console.log(add(1, 2));
--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-it("add", () => {
-  expect(add(1, 2)).toBe(3);
-});
-
-it("add", () => {
-  expect(add(1, 2)).toBe(4);
-});
